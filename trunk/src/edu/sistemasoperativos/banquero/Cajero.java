c9f55e73b8a16ea3486bb5c0ca2480c486cef273
jsecheverri@gmail.com <jsecheverri@gmail.com@0e495644-ce5e-74e6-e017-bb28c6f6de8c>
2012-01-28 22:38:55 +0000

    Clases Cliente y Banquero. Cliente implementado

---
+++ trunk/src/edu/sistemasoperativos/banquero/Cajero.java
@@ -0,0 +1,16 @@
+package edu.sistemasoperativos.banquero;
+
+import java.util.ArrayList;
+
+public class Cajero {
+
+	public boolean solicitarRecursos(ArrayList<Integer> recursosASolicitar,
+			int identificador) {
+		return true;
+	}
+
+	public void devolverRecursos(ArrayList<Integer> recursosObtenidos) {
+		
+	}
+
+}
